ce7db4c1de6c3906e90ae373bd5ca713971bf473
Kiyohito Itoh <kiyohito.itoh@gmail.com>
2020-11-17 17:20:33 +0900

    Update home screen using REST API

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Card, Text } from 'react-native-elements';
 
+import { BackendService } from '../../backend/BackendService';
+import { Trash } from '../../backend/generated-rest-client';
 import { Page, TabPage } from './TabPage';
 
 const styles = StyleSheet.create({
@@ -32,38 +34,30 @@ const styles = StyleSheet.create({
 });
 
 const Component: React.FC = () => {
+  const [point, setPoint] = useState<number>(0);
+  const [trashList, setTrashList] = useState<Trash[]>([]);
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    BackendService.getTrashList().then((response) => setTrashList(response));
+    trashList.forEach((trash) => setPoint(point + trash.point));
+  }, []);
+
   return (
     <Page>
       <Text style={styles.lead}>Let's go pick up trash!</Text>
       <Text style={styles.point}>
-        1234<Text style={styles.unit}>pt</Text>
+        {point}
+        <Text style={styles.unit}>pt</Text>
       </Text>
       <View style={styles.trashList}>
-        <Card containerStyle={styles.trash}>
-          <Card.Title>2020/11/12</Card.Title>
-          <Card.Divider />
-          <Card.Image source={{ uri: 'https://images.unsplash.com/photo-1534962303837-2edd16492781?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60' }} />
-        </Card>
-        <Card containerStyle={styles.trash}>
-          <Card.Title>2020/11/10</Card.Title>
-          <Card.Divider />
-          <Card.Image source={{ uri: 'https://images.unsplash.com/photo-1597348989645-46b190ce4918?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }} />
-        </Card>
-        <Card containerStyle={styles.trash}>
-          <Card.Title>2020/11/1</Card.Title>
-          <Card.Divider />
-          <Card.Image source={{ uri: 'https://images.unsplash.com/photo-1586045545601-36c8046cfdb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }} />
-        </Card>
-        <Card containerStyle={styles.trash}>
-          <Card.Title>2020/10/29</Card.Title>
-          <Card.Divider />
-          <Card.Image source={{ uri: 'https://images.unsplash.com/photo-1587935949075-61ceadf12587?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }} />
-        </Card>
-        <Card containerStyle={styles.trash}>
-          <Card.Title>2020/10/20</Card.Title>
-          <Card.Divider />
-          <Card.Image source={{ uri: 'https://images.unsplash.com/photo-1517664946321-87d2e16ebaa6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }} />
-        </Card>
+        {trashList.map((trash) => (
+          <Card key={trash.id} containerStyle={styles.trash}>
+            <Card.Title>{trash.date}</Card.Title>
+            <Card.Divider />
+            <Card.Image source={{ uri: trash.imageUrl }} />
+          </Card>
+        ))}
       </View>
     </Page>
   );
